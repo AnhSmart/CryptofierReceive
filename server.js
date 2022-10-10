6bd9bef139718348d15b405b0e32bb19c1a624db
SmartAnh <perry.robert489@gmail.com>
2022-10-10 09:53:33 +0300

    'f'

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+const { Storage } = require("@google-cloud/storage");
+const cors = require("cors");
+
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
+
+
 app = express();
-app.use(serveStatic(__dirname + "/client"));
+app.use(serveStatic(__dirname + "/public"));
+
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, '/client/index.html'))
 })
 
+const cloudStorage = new Storage();
+const bucketName = "test-anh";
+const bucket = cloudStorage.bucket(bucketName);
 app.get("/download", async function (req, res, next) {
-
   let filename = req.query.filename;
   res.writeHead(200, {
     "Content-Disposition": `attachment;filename=${filename}`
@@ -18,7 +27,6 @@ app.get("/download", async function (req, res, next) {
     .file(`${filename}`)
     .createReadStream() //stream is created
     .pipe(res);
-	res.send('hello');
 });
 
 var port = process.env.PORT || 5000;
