3b71073273f06441811b5a393c8f33feb72c20a1
SmartAnh <perry.robert489@gmail.com>
2022-10-10 08:59:26 +0300

    tenth

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const { Storage } = require("@google-cloud/storage");
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser")
+
 const app = express();
 const port = 81;
 const path = require("path");
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const cloudStorage = new Storage();
 const bucketName = "test-anh";
 const bucket = cloudStorage.bucket(bucketName);
