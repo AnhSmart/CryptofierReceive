b413be3330dcb70549dc6a39f2490268c581f9c4
SmartAnh <perry.robert489@gmail.com>
2022-10-10 09:12:04 +0300

    d

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ app.use(serveStatic(__dirname + "/client"));
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, '/client/index.html'))
 })
+
+app.get("/download", async function (req, res, next) {
+
+  let filename = req.query.filename;
+  res.writeHead(200, {
+    "Content-Disposition": `attachment;filename=${filename}`
+  });
+  await cloudStorage
+    .bucket(bucketName)
+    .file(`${filename}`)
+    .createReadStream() //stream is created
+    .pipe(res);
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
