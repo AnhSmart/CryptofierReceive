87ea4c735899facfe99158841013ff68e9322e43
SmartAnh <perry.robert489@gmail.com>
2022-10-10 08:50:17 +0300

    seventh

--- server.js
+++ server.js
@@ -2,25 +2,17 @@ const { Storage } = require("@google-cloud/storage");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 80;
+const port = 81;
 const path = require("path");
 
 app.use(cors());
 const cloudStorage = new Storage();
 const bucketName = "test-anh";
 const bucket = cloudStorage.bucket(bucketName);
-app.get("/download", async function (req, res, next) {
 
-  let filename = req.query.filename;
-  res.writeHead(200, {
-    "Content-Disposition": `attachment;filename=${filename}`
-  });
-  await cloudStorage
-    .bucket(bucketName)
-    .file(`${filename}`)
-    .createReadStream() //stream is created
-    .pipe(res);
-});
+app.use(express.static("/public"));
+
+
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/client/index.html"));
@@ -30,4 +22,3 @@ app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
 
-app.use(express.static("/public"));
